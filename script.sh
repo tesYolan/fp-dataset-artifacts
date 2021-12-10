7c5e0be408ea0979d05de56ec8838c16bc3845bc
Tesfa Yohannes <tmyohannes@gmail.com>
2021-12-11 01:56:59 +0800

    Final results for table-1

--- script.sh
+++ script.sh
@@ -32,10 +32,12 @@ echo "all_combined - orig - RH"
 echo "all_combined - orig - all_revisions"
 
 
-python3 run.py --do_train --max_length 50 --task nli --dataset ../counterfactually-augmented-data/NLI/original/train.tsv --output_dir /mnt/data_raid/data/val2017/torch_models-train1667 --num_train_epochs 20 --per_device_train_batch_size 16
-echo "Just train with 1667 samples taken from the revised premise"
-python3 run.py --do_train --max_length 50 --task nli --dataset ../counterfactually-augmented-data/NLI/revised_premise/train.tsv --output_dir /mnt/data_raid/data/val2017/torch_models-rp3333 --num_train_epochs 20 --per_device_train_batch_size 16
-echo "Just train with 1667 samples taken from the revised hypothesis"
+# python3 run.py --do_train --max_length 50 --task nli --dataset ../counterfactually-augmented-data/NLI/original/train.tsv --output_dir /mnt/data_raid/data/val2017/torch_models-train1667 --num_train_epochs 20 --per_device_train_batch_size 16
+# echo "Just train with 1667 samples taken from the revised premise"
+# python3 run.py --do_train --max_length 50 --task nli --dataset ../counterfactually-augmented-data/NLI/revised_premise/train.tsv --output_dir /mnt/data_raid/data/val2017/torch_models-rp3333 --num_train_epochs 20 --per_device_train_batch_size 16
+# echo "Just train with 1667 samples taken from the revised hypothesis"
 python3 run.py --do_train --max_length 50 --task nli --dataset ../counterfactually-augmented-data/NLI/revised_hypothesis/train.tsv --output_dir /mnt/data_raid/data/val2017/torch_models-rh3333 --num_train_epochs 20 --per_device_train_batch_size 16
-echo "Just train with 1667 samples taken from the all_comb"
-python3 run.py --do_train --max_length 50 --task nli --dataset ../counterfactually-augmented-data/NLI/all_combined/train.tsv --output_dir /mnt/data_raid/data/val2017/torch_models-all_comb-8331 --num_train_epochs 20 --per_device_train_batch_size 16
+# echo "Just train with 1667 samples taken from the all_comb"
+# python3 run.py --do_train --max_length 50 --task nli --dataset ../counterfactually-augmented-data/NLI/all_combined/train.tsv --output_dir /mnt/data_raid/data/val2017/torch_models-all_comb-8331 --num_train_epochs 20 --per_device_train_batch_size 16
+echo "Just train with 1667 samples taken from the revised hypothesis - revised premise"
+python3 run.py --do_train --max_length 50 --task nli --dataset ../counterfactually-augmented-data/NLI/revised_combined/train.tsv --output_dir /mnt/data_raid/data/val2017/torch_models-rhrp --num_train_epochs 20 --per_device_train_batch_size 16
--- test.sh
+++ test.sh
@@ -1,48 +1,67 @@
-# echo "orig - - orig"
-python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/original/test.tsv --model /mnt/data_raid/data/val2017/torch_models-train1667/ --output_dir results-1667-orig-orig
-echo "orig - - RH"
-python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_hypothesis/test.tsv --model /mnt/data_raid/data/val2017/torch_models-train1667/ --output_dir results-1667-orig-rh
-echo "orig - - RP"
-python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_premise/test.tsv --model /mnt/data_raid/data/val2017/torch_models-train1667/ --output_dir results-1667-orig-rp
-echo "orig - - all_revisions"
-python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_combined/test.tsv --model /mnt/data_raid/data/val2017/torch_models-train1667/ --output_dir results-1667-orig-all_combined
-echo "orig - - MNLI"
-python3 run.py --do_eval --task nli --dataset multi_nli --model /mnt/data_raid/data/val2017/torch_models-train1667/ --output_dir results-1667-orig-mnli
+# # echo "orig - - orig"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/original/test.tsv --model /mnt/data_raid/data/val2017/torch_models-train1667/ --output_dir results-1667-orig-orig
+# echo "orig - - RH"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_hypothesis/test.tsv --model /mnt/data_raid/data/val2017/torch_models-train1667/ --output_dir results-1667-orig-rh
+# echo "orig - - RP"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_premise/test.tsv --model /mnt/data_raid/data/val2017/torch_models-train1667/ --output_dir results-1667-orig-rp
+# echo "orig - - all_revisions"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_combined/test.tsv --model /mnt/data_raid/data/val2017/torch_models-train1667/ --output_dir results-1667-orig-rev_combined
+# echo "orig - - MNLI"
+# python3 run.py --do_eval --task nli --dataset multi_nli --model /mnt/data_raid/data/val2017/torch_models-train1667/ --output_dir results-1667-orig-mnli
+# echo "orig - - fp-rh"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/all_combined/test.tsv --model /mnt/data_raid/data/val2017/torch_models-train1667/ --output_dir results-1667-orig-all_combined 
 
+# echo "RP - - orig"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/original/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rp3333/ --output_dir results-1667-rp-orig
+# echo "RP - - RH"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_hypothesis/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rp3333/ --output_dir results-1667-rp-rh
+# echo "RP - - RP"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_premise/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rp3333/ --output_dir results-1667-rp-rp
+# echo "RP - - all_revisions"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_combined/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rp3333/ --output_dir results-1667-rev-all_combined 
+# echo "RP - - MNLI"
+# python3 run.py --do_eval --task nli --dataset multi_nli --model /mnt/data_raid/data/val2017/torch_models-rp3333/ --output_dir results-1667-rp-mnli
+# echo "orig - - fp-rh"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/all_combined/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rp3333/ --output_dir results-1667-rp-all_combined
 
-echo "RP - - orig"
-python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/original/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rp3333/ --output_dir results-1667-rp-orig
-echo "RP - - RH"
-python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_hypothesis/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rp3333/ --output_dir results-1667-rp-rh
-echo "RP - - RP"
-python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_premise/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rp3333/ --output_dir results-1667-rp-rp
-echo "RP - - all_revisions"
-python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_combined/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rp3333/ --output_dir results-1667-rp-all_combined
-echo "RP - - MNLI"
-python3 run.py --do_eval --task nli --dataset multi_nli --model /mnt/data_raid/data/val2017/torch_models-rp3333/ --output_dir results-1667-rp-mnli
+# echo "RH - - orig"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/original/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rh3333/ --output_dir results-1667-rh-orig
+# echo "RH - - RH"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_hypothesis/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rh3333/ --output_dir results-1667-rh-rh
+# echo "RH - - RP"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_premise/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rh3333/ --output_dir results-1667-rh-rp
+# echo "RH - - all_revisions"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_combined/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rh3333/ --output_dir results-1667-rh-all_combined
+# echo "RH - - MNLI"
+# python3 run.py --do_eval --task nli --dataset multi_nli --model /mnt/data_raid/data/val2017/torch_models-rh3333/ --output_dir results-1667-rh-mnli
+# echo "RH - - fp-rh"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/all_combined/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rh3333/ --output_dir results-1667-rh-rev-all-combined
 
-echo "RH - - orig"
-python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/original/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rh3333/ --output_dir results-1667-rh-orig
-echo "RH - - RH"
-python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_hypothesis/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rh3333/ --output_dir results-1667-rh-rh
-echo "RH - - RP"
-python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_premise/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rh3333/ --output_dir results-1667-rh-rp
-echo "RH - - all_revisions"
-python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_combined/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rh3333/ --output_dir results-1667-rh-all_combined
-echo "RH - - MNLI"
-python3 run.py --do_eval --task nli --dataset multi_nli --model /mnt/data_raid/data/val2017/torch_models-rp3333/ --output_dir results-1667-rh-mnli
+# echo "all_revisions - - orig"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/original/test.tsv --model /mnt/data_raid/data/val2017/torch_models-all_comb-8331/ --output_dir results-1667-all-orig
+# echo "all_revisions - - RH"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_hypothesis/test.tsv --model /mnt/data_raid/data/val2017/torch_models-all_comb-8331/ --output_dir results-1667-all-rh
+# echo "all_revisions - - RP"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_premise/test.tsv --model /mnt/data_raid/data/val2017/torch_models-all_comb-8331/ --output_dir results-1667-all-rp
+# echo "all_revisions - - all_revisions"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_combined/test.tsv --model /mnt/data_raid/data/val2017/torch_models-all_comb-8331/ --output_dir  results-1667-rh-rev-all-combined
+# echo "all_revisions - - MNLI"
+# python3 run.py --do_eval --task nli --dataset multi_nli --model /mnt/data_raid/data/val2017/torch_models-all_comb-8331/ --output_dir results-1667-all_combined-mnli
+# echo "all_revisions - - fp-rh"
+# python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/all_combined/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rh3333/ --output_dir results-1667-all-all_combined 
 
-echo "all_revisions - - orig"
-python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/original/test.tsv --model /mnt/data_raid/data/val2017/torch_models-all_comb-8331/ --output_dir results-1667-all-orig
-echo "all_revisions - - RH"
-python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_hypothesis/test.tsv --model /mnt/data_raid/data/val2017/torch_models-all_comb-8331/ --output_dir results-1667-all-rh
-echo "all_revisions - - RP"
-python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_premise/test.tsv --model /mnt/data_raid/data/val2017/torch_models-all_comb-8331/ --output_dir results-1667-all-rp
-echo "all_revisions - - all_revisions"
-python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_combined/test.tsv --model /mnt/data_raid/data/val2017/torch_models-all_comb-8331/ --output_dir results-1667-all-all_combined
-echo "all_revisions - - MNLI"
-python3 run.py --do_eval --task nli --dataset multi_nli --model /mnt/data_raid/data/val2017/torch_models-all_comb-8331/ --output_dir results-1667-all_combined-mnli
-
+echo "rp-rh- - orig"
+python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/original/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rhrp --output_dir results-1667-rprh-orig
+echo "rp-rh - - RH"
+python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_hypothesis/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rhrp --output_dir results-1667-rprh-rh
+echo "rp-rh- - RP"
+python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_premise/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rhrp --output_dir results-1667-rprh-rp
+echo "rp-rh- - all_revisions"
+python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/revised_combined/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rhrp --output_dir  results-1667-rprh-rev-all-combined
+echo "rp-rh - - MNLI"
+python3 run.py --do_eval --task nli --dataset multi_nli --model /mnt/data_raid/data/val2017/torch_models-rhrp --output_dir results-1667-rprh-mnli
+echo "rp-rh - - fp-rh"
+python3 run.py --do_eval --task nli --dataset ../counterfactually-augmented-data/NLI/all_combined/test.tsv --model /mnt/data_raid/data/val2017/torch_models-rhrp --output_dir results-1667-rprh-all_combined 
 # echo "MNLI -  - ORIG"
 # echo "MNLI -  - test"
 # python3 run.py --do_eval --task nli --dataset MultiNLI --model /mnt/data_raid/data/val2017/torch_models-train1667  --output_dir results-orig-mli-final/
